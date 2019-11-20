df7259ac72bac7995cedf794511f5d6ba33a6ee0
chrisshepard <43590660+chrisshepard@users.noreply.github.com>
2019-11-20 01:40:49 -0500

    nav

--- js/nav.js
+++ js/nav.js
@@ -3,8 +3,8 @@
 let nav = document.querySelector('#nav');
 let navItems = nav.querySelectorAll(".nav-item");
 navItems.forEach(el => {
-    el.addEventListener('click',()=>{
-        navItems.forEach(el=>{
+    el.addEventListener('click', () => {
+        navItems.forEach(el => {
             el.classList.remove('is-active');
         })
         el.classList.add('is-active')
@@ -14,8 +14,14 @@ navItems.forEach(el => {
 //Finds Current Route on Page Load and Updates Nav
 const initializeNav = () => {
     let currentHash = window.location.hash;
-    let loadedRoute = currentHash.substr(1);
-    document.getElementById(`nav-${loadedRoute}`).classList.add('is-active');
+    console.log(currentHash);
+    if (currentHash === "") {
+        document.getElementById('nav-home').classList.add('is-active');
+    } else {
+        let loadedRoute = currentHash.substr(1);
+        document.getElementById(`nav-${loadedRoute}`).classList.add('is-active');
+    }
+
 }
 
 initializeNav();
